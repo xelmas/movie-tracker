d9d9454e57010cb05daa2d42b3280eecf48c54d1
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2023-01-26 15:39:09 +0200

    First version of tables

--- schema.sql
+++ schema.sql
@@ -3,3 +3,40 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password TEXT
 );
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    year INTEGER
+);
+CREATE TABLE series(
+    id SERIAL PRIMARY KEY,
+    title TEXT
+);
+CREATE TABLE seasons(
+    id SERIAL PRIMARY KEY,
+    year INTEGER,
+    serie_id INTEGER REFERENCES series
+);
+CREATE TABLE ratings(
+    id SERIAL PRIMARY KEY,
+    rating INTEGER
+);
+CREATE TABLE movie_ratings (
+    id SERIAL PRIMARY KEY,
+    rating_id INTEGER REFERENCES ratings,
+    user_id INTEGER REFERENCES users,
+    movie_id INTEGER REFERENCES movies,
+);
+CREATE TABLE season_ratings (
+    id SERIAL PRIMARY KEY,
+    rating_id INTEGER REFERENCES ratings,
+    user_id INTEGER REFERENCES users,
+    season_id INTEGER REFERENCES seasons
+);
+CREATE TABLE watchlist(
+    id SERIAL PRIMARY KEY,
+    user_id REFERENCES users,
+    season_id REFERENCES seasons,
+    movie_id REFERENCES movies,
+    status INTEGER
+);
