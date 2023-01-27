001ac8d09c0a019b65306fc3773edf50b7152694
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2023-01-27 11:35:43 +0200

    Add register

---
+++ routes.py
@@ -0,0 +1,22 @@
+from app import app
+import users
+from flask import render_template, request, redirect
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Passwords are not equal")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registering not successful")
