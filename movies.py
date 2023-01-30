2b66354fe32ed2766935ef49299a1f5982b9f662
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2023-01-30 20:12:56 +0200

    Add movie

--- movies.py
+++ movies.py
@@ -6,3 +6,12 @@ def search_movie(query):
     result = db.session.execute(sql, {"query": "%"+query+"%"})
     results = result.fetchall()
     return results
+
+def add_movie(title, year):
+    try:
+        sql = "INSERT INTO movies (title, year) VALUES (:title, :year)"
+        db.session.execute(sql, {"title":title, "year":year})
+        db.session.commit()
+    except:
+        return False
+    return True
