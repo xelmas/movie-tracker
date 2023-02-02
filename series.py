dd7293eccd5f27a249434427e6a88ea43b04b9d1
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2023-02-02 17:34:53 +0200

    Add line breaks for sql queries

--- series.py
+++ series.py
@@ -2,7 +2,8 @@ from db import db
 
 
 def search_series(query):
-    sql = "SELECT title, year FROM seasons JOIN series ON seasons.serie_id = series.id WHERE title ILIKE :query"
+    sql = """SELECT title, year FROM seasons JOIN series
+             ON seasons.serie_id = series.id WHERE title ILIKE :query"""
     result = db.session.execute(sql, {"query": "%"+query+"%"})
     results = result.fetchall()
     return results
@@ -14,22 +15,29 @@ def serie_exists(title):
     return exists
 
 def season_exists(year, serie_id):
-    sql = "SELECT EXISTS (SELECT 1 FROM seasons WHERE year = :year AND serie_id=:serie_id)"
+    sql = """SELECT EXISTS (SELECT 1 FROM seasons
+             WHERE year = :year AND serie_id=:serie_id)"""
     exists = db.session.execute(sql, {"year":year,"serie_id":serie_id})
     exists = exists.fetchone()[0]
     return exists
 
 def add_serie(title, year):
-    sql = "INSERT INTO series (title) VALUES (:title) RETURNING id"
-    result = db.session.execute(sql, {"title":title})
-    serie_id = result.fetchone()[0]
-    add_season(year, serie_id)
+    try:
+        sql = "INSERT INTO series (title) VALUES (:title) RETURNING id"
+        result = db.session.execute(sql, {"title":title})
+        serie_id = result.fetchone()[0]
+        add_season(year, serie_id)
+    except:
+        return False
     return True
 
 def add_season(year, serie_id):
-    sql = "INSERT into seasons (year, serie_id) VALUES (:year, :serie_id)"
-    db.session.execute(sql, {"serie_id":serie_id, "year":year})
-    db.session.commit()
+    try:
+        sql = "INSERT into seasons (year, serie_id) VALUES (:year, :serie_id)"
+        db.session.execute(sql, {"serie_id":serie_id, "year":year})
+        db.session.commit()
+    except:
+        return False
     return True
 
 def get_serie_id(title):
@@ -39,19 +47,22 @@ def get_serie_id(title):
     return serie_id
 
 def get_season_id(year, serie_id):
-    sql = "SELECT seasons.id FROM seasons JOIN series ON seasons.serie_id = series.id WHERE year =:year AND series.id = :serie_id"
+    sql = """SELECT seasons.id FROM seasons JOIN series ON seasons.serie_id = series.id
+             WHERE year =:year AND series.id = :serie_id"""
     result = db.session.execute(sql, {"year":year, "serie_id":serie_id})
     season_id = result.fetchone()[0]
     return season_id
 
 def add_watchlist(user_id, season_id):
 
-    sql = "SELECT EXISTS (SELECT 1 FROM series_watchlist WHERE season_id = :season_id AND user_id=:user_id)"
+    sql = """SELECT EXISTS (SELECT 1 FROM series_watchlist
+             WHERE season_id = :season_id AND user_id=:user_id)"""
     exists = db.session.execute(sql, {"season_id":season_id, "user_id":user_id})
     exists = exists.fetchone()[0]
 
     if not exists:
-        sql = "INSERT INTO series_watchlist (user_id, season_id) VALUES (:user_id, :season_id)"
+        sql = """INSERT INTO series_watchlist (user_id, season_id)
+                 VALUES (:user_id, :season_id)"""
         db.session.execute(sql, {"user_id":user_id, "season_id":season_id})
         db.session.commit()
         return True
@@ -67,20 +78,28 @@ def delete_watchlist(user_id, season_id):
     return True
 
 def mark_watched(season_id, user_id):
-    sql = "UPDATE series_watchlist SET status=1 WHERE series_watchlist.season_id=:season_id AND series_watchlist.user_id=:user_id"
-    db.session.execute(sql, {"season_id":season_id, "user_id":user_id})
-    db.session.commit()
+    try:
+        sql = """UPDATE series_watchlist SET status=1 WHERE series_watchlist.season_id=:season_id
+                AND series_watchlist.user_id=:user_id"""
+        db.session.execute(sql, {"season_id":season_id, "user_id":user_id})
+        db.session.commit()
+    except:
+        return False
     return True
 
 def watched(user_id):
 
-    sql = "SELECT t.id, title, year, t.media FROM (SELECT S.id, title, year, media FROM seasons AS S JOIN series ON S.serie_id=series.id) AS t JOIN series_watchlist ON t.id=series_watchlist.season_id WHERE status = 1 AND user_id=:user_id"
+    sql = """SELECT t.id, title, year, t.media FROM (SELECT S.id, title, year, media FROM seasons AS S
+             JOIN series ON S.serie_id=series.id) AS t JOIN series_watchlist ON t.id=series_watchlist.season_id
+             WHERE status = 1 AND user_id=:user_id"""
     result = db.session.execute(sql, {"user_id":user_id})
     watched_series = result.fetchall()
     return watched_series
 
 def watchlist(user_id):
-    sql = "SELECT t.id, title, year, t.media, status FROM (SELECT S.id, title, year, media FROM seasons AS S JOIN series ON S.serie_id=series.id) AS t JOIN series_watchlist ON t.id=series_watchlist.season_id WHERE status = 0 AND user_id=:user_id"
+    sql = """SELECT t.id, title, year, t.media, status FROM (SELECT S.id, title, year, media FROM seasons AS S
+             JOIN series ON S.serie_id=series.id) AS t JOIN series_watchlist ON t.id=series_watchlist.season_id
+             WHERE status = 0 AND user_id=:user_id"""
     result = db.session.execute(sql, {"user_id":user_id})
     series_watchlist = result.fetchall()
     return series_watchlist
